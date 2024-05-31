0d7e3d124fb6fabe57e52d53677275713c185d4f
alawii17 <alawialatas1414@gmail.com>
2024-05-31 08:40:43 +0700

    merubah port

--- codecept.conf.js
+++ codecept.conf.js
@@ -13,7 +13,7 @@ exports.config = {
   helpers: {
     Playwright: {
       browser: 'chromium',
-      url: 'http://localhost:9001',
+      url: 'http://localhost:8081',
       show: true,
     },
   },
