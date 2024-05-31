bef454e9d0c4ef24d3dd9e0043ea12ad69cf4e96
alawii17 <alawialatas1414@gmail.com>
2024-05-31 13:49:28 +0700

    revisi

--- codecept.conf.js
+++ codecept.conf.js
@@ -13,7 +13,7 @@ exports.config = {
   helpers: {
     Playwright: {
       browser: 'chromium',
-      url: 'http://localhost:8081',
+      url: 'http://localhost:9000',
       show: true,
     },
   },
--- webpack.dev.js
+++ webpack.dev.js
@@ -9,7 +9,7 @@ module.exports = merge(common, {
     static: path.resolve(__dirname, 'dist'),
     open: true,
     compress: true,
-    port: 8081,
+    port: 9000,
     client: {
       overlay: {
         errors: true,
