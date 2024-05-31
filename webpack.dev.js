8eea8252fecf4edd315dd0bb718a40fe0292de59
alawii17 <alawialatas1414@gmail.com>
2024-05-31 08:39:58 +0700

    merubah port

--- webpack.dev.js
+++ webpack.dev.js
@@ -9,7 +9,7 @@ module.exports = merge(common, {
     static: path.resolve(__dirname, 'dist'),
     open: true,
     compress: true,
-    port: 9001,
+    port: 8081,
     client: {
       overlay: {
         errors: true,
